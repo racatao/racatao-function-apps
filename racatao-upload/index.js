5dd1882263deef3460d5e1042388d1c779acd5b2
Gustavo Aguiar <gugahoa@gmail.com>
2017-10-28 07:06:42 -0200

    Move context.done inside handler and else clause

--- racatao-upload/index.js
+++ racatao-upload/index.js
@@ -11,14 +11,12 @@ module.exports = function (context, req) {
         };
 
         var multipartData = new MultipartParser(req, context);
-		    context.log(multipartData);
-        context.bindings.uploadBlob = multipartData;
     }
     else {
         context.res = {
             status: 400,
             body: "Please pass a name on the query string or in the request body"
         };
+        context.done();
     }
-    context.done();
 };

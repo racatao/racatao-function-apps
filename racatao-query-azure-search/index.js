6c44e989ac34861074235b7a1648a571c58b4b1a
Gustavo Aguiar <gugahoa@gmail.com>
2017-10-28 22:07:42 -0200

    Create elastic search consult API

---
+++ racatao-query-azure-search/index.js
@@ -0,0 +1,40 @@
+const rp = require('request-promise')
+// query-key 5112AF0AAE64FF3EE9428CB0794C8034
+
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.body && req.body.search) {
+      const search = req.body.search
+      rp({
+        method: 'GET',
+        url: 'https://racatao-documents.search.windows.net/indexes/raw-files-index/docs',
+        qs: {
+          search,
+          'api-key': '5112AF0AAE64FF3EE9428CB0794C8034',
+          'api-version': '2016-09-01'
+        }
+      })
+      .then((response) => {
+        context.res = {
+          status: 200,
+          body: {
+            data: response.value.map((el) => el.metadata_storage_name),
+            count: response.value.length
+          },
+          headers: {
+            content_type: 'application/json'
+          }
+        }
+        context.log(response.value.length, response.value)
+      })
+      .catch((err) => {
+        context.res = {
+          status: 400,
+          body: "Error querying AzureSearch API"
+        }
+        context.log('err', err)
+      })
+    }
+    context.done();
+};
